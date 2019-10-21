55cf503ff01933fdad07db73bc4fe00b04df964f
adanino2 <benabdelaaliadnen@gmail.com>
2019-10-21 12:12:09 +0100

    work done

---
+++ Abdo.js
@@ -0,0 +1,175 @@
+// Use the following helper functions in your solution
+function each(coll, f) {
+  if (Array.isArray(coll)) {
+    for (var i = 0; i < coll.length; i++) {
+      f(coll[i], i);
+    }
+  } else {
+    for (var key in coll) {
+      f(coll[key], key);
+    }
+  }
+}
+
+function filter(array, predicate) {
+  var acc = [];
+  each(array, function(element, i) {
+    if (predicate(element, i)) {
+      acc.push(element);
+    }
+  });
+  return acc;
+}
+
+function map(array, func) {
+  var acc = [];
+  each(array, function(element, i) {
+    acc.push(func(element, i));
+  });
+  return acc;
+}
+
+// Remember to relax :)
+
+//=======================================================================
+/*                                  Q1                                 */
+//=======================================================================
+
+/*
+Depending on data modeling concept that you have learned:
+
+Create a factory function called makeComputer that represents computers, 
+What different attributes computers may have? 
+Create two computers object from your factory function and save them in one array called computers!
+
+Note: please write one or two lines here describing your solution.
+*/
+
+function makeComputer(type, color, weight) {
+  // TODO: Your code here
+  var computers = {};
+  return computer = {type: "type", color: "color", weight: "weight"};
+}
+var computers = [];
+computers[0]= makeComputer("Tohiba" , "black", 32); 
+computers[1]= makeComputer("dell" , "grey", 30); 
+
+
+// Write a function displayComputer that can be used to display one computer.
+
+function displayComputer(computer) {
+  // TODO: Your code here
+ return makeComputer(computer) ;
+}
+
+//=============================================================================
+/*                                  Q2                                       */
+//=============================================================================
+
+/*
+Write a function that takes an array of strings as a input
+and returns an array of all of those strings, but transformed to upper case.
+
+You can use toUpperCase method to convert a string to upper case. 
+Solve it using the most appropriate helper functions(each,map,filter).
+
+Note: please write one or two lines here describing your solution.
+
+var strArr = ['hello', 'world', 'whirled', 'peas'];
+uppercaseAll(strArr); ==> [ 'HELLO', 'WORLD', 'WHIRLED', 'PEAS' ]
+*/
+
+function uppercaseAll(arrayOfStrings) {
+  // TODO: your code here
+ ;
+var upperCase= [];
+for(var i =0 ;i< arrayOfStrings.length ; i++){
+   upperCase[i]= arrayOfStrings[i].toUpperCase();
+}
+ return upperCase;
+}
+uppercaseAll(["Hello","world","Peas"]);
+//=============================================================================
+/*                                  Q3                                       */
+//=============================================================================
+
+/*
+Write a function that takes array of objects as an input and returns an array
+with only the countries that have a population higher than 500 million.
+
+Solve it using one of the most appropriate helperthe helpers functions(each,map,filter).
+
+highestPopulation(data); ==> [{country: "China", population: 1409517397},{country: "India", population: 1339180127}]
+Note: please write one or two lines here describing your solution.
+
+Here’s the data you’ll work with:
+*/
+
+var data = [
+  {
+    country: 'China',
+    population: 1409517397
+  },
+  {
+    country: 'India',
+    population: 1339180127
+  },
+  {
+    country: 'USA',
+    population: 324459463
+  },
+  {
+    country: 'Indonesia',
+    population: 263991379
+  }
+];
+
+function highestPopulation(arrayOfObjects) {
+  // TODO: your code here
+  var pop = [] ;
+  var c = 0;
+  for( var i= 0 ; i< arrayOfObjects.length; i++){
+  	if(arrayOfObjects[i]["population"] > 500000000){
+  		pop[c] = arrayOfObjects[i];
+  		c++;
+  	}
+  } return pop;
+}
+
+//=============================================================================
+/*                              Q4                                           */
+//=============================================================================
+
+/*
+Write a function halveAll that takes an array of numbers as a input 
+and returns an array of all of those numbers halved (divided by two).
+
+Note: solve it using the most appropriate helper functions(each,map,filter)
+
+var numsArray = [2, 6, 20, 8, 14];
+halveAll(numsArray); ==> [ 1, 3, 10, 4, 7 ]
+
+Note: please write one or two lines here describing your solution.
+*/
+
+function halveAll(numbers) {
+  // your code is here
+}
+
+//=============================================================================
+/*                                  Q5                                       */
+//=============================================================================
+
+/*
+Write a function called values that accepts an object as a parameter, and outputs an array of the object's values. 
+Solve it using one of the most appropriate helpers functions(each,map,filter).
+
+values({first : 5, second: 'something' , third : 129}) ==> [5, 'something', 129];
+
+Note: please write one or two lines here describing your solution.
+*/
+function values(obj) {
+  // TODO: your code here
+}
+
+//Good Luck :))
